f35cb313616947a4c2c2232c7507b173b0749b31
jarrisonfor <40749014+jarrisonfor@users.noreply.github.com>
2020-02-07 18:38:47 +0000

    Update and rename smb.sh to LDAP a SMB.sh

--- LDAP a SMB.sh
+++ LDAP a SMB.sh
@@ -3,7 +3,6 @@ slapcat | grep "uid: " | cut -d" " -f2 > tmp.txt
 
 while read linea
 do
-        echo "$linea"
         useradd $linea
         (echo "smr1234"; echo "smr1234") | smbpasswd -a -s $linea
 done < tmp.txt
