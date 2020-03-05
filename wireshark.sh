172ba882245c5d82dfe56374b2b46a839ead65ad
jarrisonfor <40749014+jarrisonfor@users.noreply.github.com>
2020-03-05 23:01:46 +0000

    Delete wireshark.sh

--- wireshark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# script para usar wireshark sin sudo
-
-sudo apt update
-sudo apt install wireshark -y
-sudo dpkg-reconfigure wireshark-common
-sudo chmod 777 /usr/bin/dumpcap
-sudo chmod 777 ~/.config/wireshark/*
