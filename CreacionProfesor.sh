4460a14a4bcd5a27984dcbb88cd45f29ebdd8e5e
jarrisonfor <40749014+jarrisonfor@users.noreply.github.com>
2020-03-20 17:39:53 +0000

    creacion y borrado de usuarios individuales

--- CreacionProfesor.sh
+++ CreacionProfesor.sh
@@ -8,8 +8,9 @@ case "$1" in
         sudo a2dissite 000-default.conf
         sudo a2ensite default-ssl.conf
         sudo a2enmod userdir rewrite headers ssl
-        sudo quotacheck -vagum -f
+        sudo quotacheck -a -g -u -m -c -f
         
+        sudo service mysql restart
         sudo mysql_secure_installation
         read -p 'Contraseña para el mysql: ' password
         sudo mysql -u root -e "CREATE DATABASE $USER"
@@ -36,7 +37,6 @@ case "$1" in
         sudo chown :www-data ~/public_html -R
         sudo chown www-data:www-data /var/www/html/* -R
         sudo chmod u+rwx,g+rwxs,o-rwx /etc/skel/* -R
-        sudo chmod 700 ~/bin/
         sudo chmod u+rwx,g+rwxs ~/public_html -R
         
         sudo sed -i '/#/ d' /etc/apache2/sites-available/default-ssl.conf
@@ -46,8 +46,13 @@ case "$1" in
         sudo sed -i "/blowfish_secret/ c \$cfg['blowfish_secret'] = 'iVkv2U2E}E9bgpGWhUg-DpYUQf;h7rxN';" /var/www/html/phpmyadmin/config.inc.php
         sudo sed -i "/php_admin_flag engine Off/ c #php_admin_flag engine Off" /etc/apache2/mods-available/php7.*.conf
         sudo sed -i 's/defaults/defaults,usrquota,grpquota/g' /etc/fstab
+        
+        clear
+        echo "Se reiniciara la maquina para recargar los cambios"
+        sleep 10
+        sudo reboot
     ;;
-    -c)
+    -cl)
         # creacion de usuarios leyendo archivo
         while read linea
         do
@@ -66,10 +71,10 @@ case "$1" in
         sudo chown $USER /home/*
         
         echo "Se reiniciara la maquina para recargar los cambios"
-        sleep 10
+        sleep 20
         sudo reboot
     ;;
-    -b)
+    -bl)
         # borrado de usuarios leyendo archivo
         sudo service mysql restart
         while read linea
@@ -85,10 +90,42 @@ case "$1" in
         done < $2
         
         echo "Se reiniciara la maquina para recargar los cambios"
-        sleep 10
+        sleep 20
         sudo reboot
     ;;
+    -cu)
+        # creacion de un usuario individual
+        alumno=$2
+        password="csas1234"
+        sudo useradd $alumno -m -s /bin/bash -K UMASK=007
+        echo -e "$password\n$password\n" | sudo passwd $alumno &> /dev/null
+        sudo usermod -a -G $(id -g $alumno) $USER
+        sudo usermod -a -G $(id -g $alumno) www-data
+        sudo mysql -u root -e "CREATE DATABASE $alumno"
+        sudo mysql -u root -e "CREATE USER '$alumno'@'%' IDENTIFIED BY '$password'"
+        sudo mysql -u root -e "GRANT ALL PRIVILEGES ON $alumno.* TO '$alumno'@'%'"
+        sudo setquota -u $alumno 524288 1048576 0 0 /
+        # Permisos
+        sudo chown $USER /home/*
+        
+        echo "se aconseja reiniciar la maquina para que los cambios tengan efecto"
+    ;;
+    -bu)
+        # borrado de un usuario individual
+        sudo service mysql restart
+        
+        alumno=$2
+        sudo killall -u $alumno
+        sudo deluser --remove-home -f $alumno &> /dev/null
+        sudo gpasswd -d $USER $alumno &> /dev/null
+        sudo gpasswd -d www-data $alumno &> /dev/null
+        sudo delgroup $alumno &> /dev/null
+        sudo mysql -u root -e "DROP DATABASE $alumno"
+        sudo mysql -u root -e "DROP USER '$alumno'@'%'"
+        
+        echo "se aconseja reiniciar la maquina para que los cambios tengan efecto"
+    ;;
     *)
-        echo -e "Parametros permitidos: \n -i: Instalar todo lo necesario (solo la primera vez) \n -c: Crear los usuarios en lote ej: script.sh -c listaalumnos.txt \n -b: Borrar los usuarios en lote ej: script.sh -b listaalumnos.txt"
+        echo -e "Parametros permitidos: \n -i: Instalar todo lo necesario (solo la primera vez) \n -cl: Crear los usuarios en lote ej: script.sh -cl listaalumnos.txt \n -bl: Borrar los usuarios en lote ej: script.sh -bl listaalumnos.txt \n -cu: Crea un usuario ej: script.sh -cu pepito \n -bu: Borrar un usuario ej: script.sh -bu pepito"
     ;;
 esac
